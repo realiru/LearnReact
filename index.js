14e1f85b2a67dd551b1d6c96b979d6c737c897b5
Hamzah Patel <hamzahpatel403@gmail.com>
2023-12-30 22:36:41 +1300

    Challenge
    - Move the `footer` into its own component called Footer"  and render that component inside the Page component.
    - Move the `h1` and `ol` together into another component called "MainContent" and render inside Page as well.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 /**
-Mini Challenge:
-Move the `header` element from Page into 
-its own component called "Header"
+Challenge: 
+
+- Move the `footer` into its own component called "Footer" 
+  and render that component inside the Page component.
+- Move the `h1` and `ol` together into another component
+  called "MainContent" and render inside Page as well.
 */
 
 function Header(){
