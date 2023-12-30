628d344f467be3dec3dcf136efb1e8132721d8eb
Hamzah Patel <hamzahpatel403@gmail.com>
2023-12-30 22:35:31 +1300

    Mini Challenge Completed
    Completed challenge by creating Header function and calling it inside of Page function.

--- index.js
+++ index.js
@@ -4,14 +4,20 @@ Move the `header` element from Page into
 its own component called "Header"
 */
 
+function Header(){
+    return (
+        <header>
+        <nav>
+            <img src="./react-logo.png" width="40px" />
+        </nav>
+    </header>
+    )
+}
+
 function Page() {
   return (
       <div>
-          <header>
-              <nav>
-                  <img src="./react-logo.png" width="40px" />
-              </nav>
-          </header>
+        <Header />
           <h1>Reasons I'm excited to learn React</h1>
           <ol>
               <li>It's a popular library, so I'll be 
@@ -20,7 +26,7 @@ function Page() {
               if I know React</li>
           </ol>
           <footer>
-              <small>© 2021 Ziroll development. All rights reserved.</small>
+              <small>© 2021 Patel development. All rights reserved.</small>
           </footer>
       </div>
   )
