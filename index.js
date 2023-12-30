629bb33ba8f28d5f0b017dbb0141aad6b062fe29
Hamzah Patel <hamzahpatel403@gmail.com>
2023-12-30 22:45:50 +1300

    Challenge Completed
    Created two new functions - Footer and MainContent and called them inside Page function.
    
    Was able to understand how parent components interact and contain multiple components.

--- index.js
+++ index.js
@@ -17,20 +17,34 @@ function Header(){
     )
 }
 
-function Page() {
+function Footer(){
+    return (
+        <footer>
+            <small>© 2021 Patel development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+function MainContent(){
   return (
-      <div>
-        <Header />
-          <h1>Reasons I'm excited to learn React</h1>
+    <div>
+    <h1>Reasons I'm excited to learn React</h1>
           <ol>
               <li>It's a popular library, so I'll be 
               able to fit in with the cool kids!</li>
               <li>I'm more likely to get a job as a developer
               if I know React</li>
           </ol>
-          <footer>
-              <small>© 2021 Patel development. All rights reserved.</small>
-          </footer>
+    </div>
+  )
+}
+
+function Page() {
+  return (
+      <div>
+        <Header />
+          <MainContent />
+          <Footer />
       </div>
   )
 }
