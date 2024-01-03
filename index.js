dc4588a0ca01a9c9aff3b3c463d82727cc66e084
Hamzah Patel <hamzahpatel403@gmail.com>
2024-01-04 01:16:49 +1300

    Setup for 1.21
    Setup files for Scrimba Course 1 Lesson 21: Styling with classes

--- index.js
+++ index.js
@@ -1,48 +1,47 @@
+
 /**
 Challenge: 
 
-- Move the `footer` into its own component called "Footer" 
-  and render that component inside the Page component.
-- Move the `h1` and `ol` together into another component
-  called "MainContent" and render inside Page as well.
+- Add an `ul` inside the Header's `nav` and create
+  the following `li`s: "Pricing", "About", & "Contact"
 */
 
-function Header(){
-    return (
-        <header>
-        <nav>
-            <img src="./react-logo.png" width="40px" />
-        </nav>
-    </header>
-    )
-}
-
-function Footer(){
-    return (
-        <footer>
-            <small>© 2021 Patel development. All rights reserved.</small>
-        </footer>
-    )
-}
-
-function MainContent(){
+function Header() {
   return (
-    <div>
-    <h1>Reasons I'm excited to learn React</h1>
+      <header>
+          <nav>
+              <img src="./react-logo.png" width="40px" />
+          </nav>
+      </header>
+  )
+}
+
+function Footer() {
+  return (
+      <footer>
+          <small>© 2021 Patel development. All rights reserved.</small>
+      </footer>
+  )
+}
+
+function MainContent() {
+  return (
+      <div>
+          <h1>Reasons I'm excited to learn React</h1>
           <ol>
               <li>It's a popular library, so I'll be 
               able to fit in with the cool kids!</li>
               <li>I'm more likely to get a job as a developer
               if I know React</li>
           </ol>
-    </div>
+      </div>
   )
 }
 
 function Page() {
   return (
       <div>
-        <Header />
+          <Header />
           <MainContent />
           <Footer />
       </div>
