58c2ca2dd2aec641610e3a383c0f36669ed3839d
Hamzah Patel <hamzahpatel403@gmail.com>
2024-01-05 02:01:20 +1300

    Completed Challenge Successfully
    Using Vite the challenge was able to be completed.
    Header, Footer and MainContent are moved into their own files (.jsx)

--- Learn-React/src/App.jsx
+++ Learn-React/src/App.jsx
@@ -1,42 +1,12 @@
 import reactLogo from './assets/react.svg'
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+import MainContent from './MainContent.jsx'
 
-
-function Header() {
-  return (
-      <header>
-          <nav className="nav">
-          <img src={reactLogo} className="nav-logo"  alt='React Logo'/>
-            <ul className="nav-items">
-              <li>Pricing</li>
-              <li>About</li>
-              <li>Contact</li>
-            </ul>
-          </nav>
-      </header>
-  )
-}
-
-function Footer() {
-  return (
-      <footer>
-          <small>© 2021 Patel development. All rights reserved.</small>
-      </footer>
-  )
-}
-
-function MainContent() {
-  return (
-      <div>
-          <h1>Reasons I'm excited to learn React</h1>
-          <ol>
-              <li>It's a popular library, so I'll be 
-              able to fit in with the cool kids!</li>
-              <li>I'm more likely to get a job as a developer
-              if I know React</li>
-          </ol>
-      </div>
-  )
-}
+/**
+Challenge: move the Footer and MainContent components
+into their own files.
+*/
 
 function App() {
   return (
