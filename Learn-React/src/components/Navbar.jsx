35aeea77de558cec15bfb308a53ca73817f1c64c
Hamzah Patel <hamzahpatel403@gmail.com>
2024-01-09 16:44:25 +1300

    Attempt at Navbar
    Solo attempt at designing the navbar to match the example provided.
    Outcome: Visibly similar but I feel I used a lot of bad syntax and over coded simple things.

--- Learn-React/src/components/Navbar.jsx
+++ Learn-React/src/components/Navbar.jsx
@@ -1,5 +1,15 @@
 import React from "react";
-
+import Logo from '../assets/react.svg';
 export default function Navbar() {
-return (<h1 className="navbar">Navbar goes here</h1>)
+return (
+<nav className="navbar"> 
+<div className="navbar-right">
+<img className="navbar-logo" src={Logo} alt="Logo"/>
+<h1 className="navbar-text">ReactFacts</h1>
+</div>
+<div className="navbar-left">
+<p className="navbar-subtext"> React Course - Project 1</p>
+</div>
+</nav>
+)
 }
