8f298597d93a41242354cfc0f9a07a0b34800979
Hamzah Patel <hamzahpatel403@gmail.com>
2024-01-21 16:48:16 +1300

    Added text
    Added text and css for the name and personal info below the picture.
    Have to add fonts for use

--- Project - Digital Business Card/Business Card/src/App.jsx
+++ Project - Digital Business Card/Business Card/src/App.jsx
@@ -1,12 +1,14 @@
 import Photo from './assets/photo.png';
-
+import PersonalInfo from './components/PersonalInfo';
 function App() {
   return(
-  <div className="container">
-  {/* Need to change photo once better picture is available */}
-  <img src={Photo} className="self-photo"/> 
-
-  </div>
+    <div className="container">
+    {/* Need to change photo once better picture is available */}
+    <img src={Photo} className="self-photo"/> 
+    <div className="text--container">
+    <PersonalInfo />
+    </div>
+    </div>
   )
 }
 
--- Project - Digital Business Card/Business Card/src/components/PersonalInfo.jsx
+++ Project - Digital Business Card/Business Card/src/components/PersonalInfo.jsx
@@ -1,2 +1,9 @@
 import React from "react";
 // Add name, role, website
+export default function PersonalInfo() {
+    return (
+        <div className="personalinfo">
+            <h1>test</h1>
+        </div>
+    )
+}
