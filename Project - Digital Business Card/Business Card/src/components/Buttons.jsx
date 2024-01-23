2c8972435b7b60a2354c2a7bbbceaf3d98262cf4
Hamzah Patel <hamzahpatel403@gmail.com>
2024-01-23 17:14:53 +1300

    Added icons
    Added icons to the button, have to align and style text correctly

--- Project - Digital Business Card/Business Card/src/components/Buttons.jsx
+++ Project - Digital Business Card/Business Card/src/components/Buttons.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Mail from '../assets/Mail.svg';
+import Linkedin from '../assets/Linkedin.svg';
 
 export default function Buttons() {
     return (
         <div className="buttons">
-            <button className="buttons--email"><h1>Email</h1></button>
-            <button className="buttons--linkedin">Linkedin</button>
+            <button className="buttons--email">
+                <p><img src={Mail}/>Test</p>
+                </button>
+            <button className="buttons--linkedin">
+                <p> <img src={Linkedin}/>Test</p>
+                </button>
             </div>
     )
     }
