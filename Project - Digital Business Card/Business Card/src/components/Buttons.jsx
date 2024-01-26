3b15f1e7232bd9502a89498a8e8d8508a9ccd9d9
Hamzah Patel <hamzahpatel403@gmail.com>
2024-01-26 15:07:01 +1300

    Finished buttons v2
    Finished buttons component - made them reactive to pressing and link to the correct pages

--- Project - Digital Business Card/Business Card/src/components/Buttons.jsx
+++ Project - Digital Business Card/Business Card/src/components/Buttons.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import Mail from '../assets/Mail.svg';
 import Linkedin from '../assets/Linkedin.svg';
 
+
 export default function Buttons() {
+    const handleEmailClick = () => {
+        window.location.href = 'mailto:Hamzahpatel403@gmail.com';
+    };
+    const handleLinkedinClick = () => {
+        window.open('https://www.linkedin.com/in/hamzahpatel/','_blank');
+    }
     return (
         <div className="buttons">
-            <button className="buttons--email">
+            <button onClick={handleEmailClick} className="buttons--email">
                 <img src={Mail}/><p>Email</p>
                 </button>
-            <button className="buttons--linkedin">
+            <button onClick={handleLinkedinClick} className="buttons--linkedin">
                 <img src={Linkedin}/><p>LinkedIn</p>
                 </button>
             </div>
