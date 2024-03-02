e0ac47282ef9f456140753ec863fba6ed19571ca
Hamzah Patel <hamzahpatel403@gmail.com>
2024-03-02 13:07:38 +1300

    Static Card component completed
    Card component completed in a static way

--- Learn-React/Module 2/src/App.jsx
+++ Learn-React/Module 2/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import Card from "./components/Card"
 /*
 Challenge: Build the Hero component.
 Check the Figma file for the design specifics.
@@ -10,7 +11,8 @@ export default function App() {
     return (
       <div>
         <Navbar/>
-        <Hero/>
+        {/* <Hero/> */}
+        <Card/>
       </div>
     )
 }
