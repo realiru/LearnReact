6fd290b4a81474cb1bce874cb556028fbf242b04
Hamzah Patel <hamzahpatel403@gmail.com>
2024-03-04 11:33:25 +1300

    worked on card component

--- Learn-React/Module 2/src/components/Card.jsx
+++ Learn-React/Module 2/src/components/Card.jsx
@@ -22,14 +22,14 @@ Notes:
 export default function Card() {
     return (
       <div className="card">
-        <img src="src/assets/katie-zaferes.png" className="card-image" />
+        <img src="src/assets/katie-zaferes.png" className="card-image" alt="Main image card." />
         <div className="card-stats">
-            <img src="src/assets/star.png" />
+            <img src="src/assets/star.png" alt="Star icon."/>
             <span>5.0</span>
             <span className="gray">(6) • </span>
             <span className="gray">USA</span>
         </div>
-        <p>Life Lessons with Katie Zaferes</p>
+        <h2>Life Lessons with Katie Zaferes</h2>
         <p><span className="bold">From $136</span> / person</p>
       </div>
     )
